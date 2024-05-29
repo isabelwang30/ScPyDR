20c6489e3a6e8e6c4c01d537c7a3ae306fd5bed8
asheorann <114448991+asheorann@users.noreply.github.com>
2024-05-28 22:10:19 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+# Version-keeping code
+curdir = os.path.abspath(os.path.dirname(__file__))
+MAJ = 0
+MIN = 0
+REV = 0
+VERSION = '%d.%d.%d' % (MAJ, MIN, REV)
+with open(os.path.join(curdir, 'scPyDR/version.py'), 'w') as fout:
+    fout.write(
+        "\n".join(["",
+                   "# THIS FILE IS GENERATED FROM SETUP.PY",
+                   "version = '{version}'",
+                   "__version__ = version"]).format(version=VERSION)
+    )
+
+# Setup function
+setup(
+    name='scPyDR',
+    version=VERSION,
+    description='CSE185 Final Project',
+    author='Anushka Sheoran, Isabel Wang, Monica Park',
+    author_email='asheoran@ucsd.edu, ijwang@ucsd.edu, mepark@ucsd.edu',
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "scpyDR=scPyDR.scPyDR:main"  # Entry point for command-line script
+        ],
+    },
+)
