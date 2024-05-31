97a43247bb9964339c1150f670af511f52c99e97
isabelwang30 <103291789+isabelwang30@users.noreply.github.com>
2024-05-31 13:01:07 -0700

    Update scPyDR.py
    updated error handling

--- scPyDR/scPyDR.py
+++ scPyDR/scPyDR.py
@@ -20,102 +20,108 @@ def main():
     # -------------------- parse command-line arguments into program --------------------
 
     parser = argparse.ArgumentParser(
-        prog = "scPyDR", \
-        description = "Command-line script to reduce the dimensionality and visualize 10x Genomics scRNA-seq data"
+        prog="scPyDR",
+        description="Command-line script to reduce the dimensionality and visualize 10x Genomics scRNA-seq data"
     )
 
     # input (required)
-    parser.add_argument("datadir", \
-                        help = "Directory containing 10x Genomics scRNA-seq data files.", \
-                        metavar="DIR", \
-                        type = str)
+    parser.add_argument("datadir",
+                        help="Directory containing 10x Genomics scRNA-seq data files.",
+                        metavar="DIR",
+                        type=str)
 
     # output (optional)
-    parser.add_argument("-o", "--output", \
-                        help = "Output directory to store results. Default: working directory.", \
-                        type = str, \
-                        required = False)
-    
+    parser.add_argument("-o", "--output",
+                        help="Output directory to store results. Default: working directory.",
+                        type=str,
+                        required=False)
+
     # other options (optional)
-    parser.add_argument("-g", "--min_genes", \
-                        help = "Minimum number of genes expressed per cell. Default: 200.", \
-                        type = int, \
-                        required = False)
-    parser.add_argument("-c", "--min_cells", \
-                        help = "Minimum number of cells expressing a gene. Default: 5.", \
-                        type = int, \
-                        required = False)
-    parser.add_argument("-cr", "--min_cell_reads", \
-                        help = "Minimum number of reads per cell. Default: None.", \
-                        type = int, \
-                        required = False)
-    parser.add_argument("-gc", "--min_gene_counts", \
-                        help = "Minimum number of counts per gene. Default: None.", \
-                        type = int, \
-                        required = False)
-    parser.add_argument("-ntop", "--n_top_genes", \
-                        help = "Number of highly variable genes to keep. Default: 500.", \
-                        type = int, \
-                        required = False)
-    parser.add_argument("-t", "--target_sum", \
-                        help = "Number of reads per cell for normalization. Default: 1e4.", \
-                        type = float, \
-                        required = False)
-    parser.add_argument("-n", "--nComp", \
-                        help = "Number of principal componenets. Default: for n data points and m features, there are min(n-1,m) PCs.", \
-                        type = int, \
-                        required = False)
-    parser.add_argument("--version", \
-                        help = "Print the version of scPyDR.", \
-                        action = "version", \
-                        version = '{version}'.format(version = __version__))
-    parser.add_argument("-u", "--umap", 
-                        help = "Run UMAP for dimensionality reduction and visualization.", 
-                        action="store_true", 
-                        required = False)
-    parser.add_argument("-v", "--visualize", 
-                    help="Visualize the results of scPyDR.", 
-                    action="store_true", 
-                    required=False)
+    parser.add_argument("-g", "--min_genes",
+                        help="Minimum number of genes expressed per cell. Default: 200.",
+                        type=int,
+                        required=False)
+    parser.add_argument("-c", "--min_cells",
+                        help="Minimum number of cells expressing a gene. Default: 5.",
+                        type=int,
+                        required=False)
+    parser.add_argument("-cr", "--min_cell_reads",
+                        help="Minimum number of reads per cell. Default: None.",
+                        type=int,
+                        required=False)
+    parser.add_argument("-gc", "--min_gene_counts",
+                        help="Minimum number of counts per gene. Default: None.",
+                        type=int,
+                        required=False)
+    parser.add_argument("-ntop", "--n_top_genes",
+                        help="Number of highly variable genes to keep. Default: 500.",
+                        type=int,
+                        required=False)
+    parser.add_argument("-t", "--target_sum",
+                        help="Number of reads per cell for normalization. Default: 1e4.",
+                        type=float,
+                        required=False)
+    parser.add_argument("-n", "--nComp",
+                        help="Number of principal components. Default: for n data points and m features, there are min(n-1,m) PCs.",
+                        type=int,
+                        required=False)
+    parser.add_argument("--version",
+                        help="Print the version of scPyDR.",
+                        action="version",
+                        version='{version}'.format(version=__version__))
+    parser.add_argument("-u", "--umap",
+                        help="Run UMAP for dimensionality reduction and visualization.",
+                        action="store_true",
+                        required=False)
+    parser.add_argument("-v", "--visualize",
+                        help="Visualize the results of scPyDR.",
+                        action="store_true",
+                        required=False)
 
     # parse args
     args = parser.parse_args()
-    
+
     # validate args
     if not os.path.isdir(args.datadir):
-        print("Invalid directory path to 10x Genomics data files.")
-        parser.print_help()
-    if not os.path.isdir(args.output):
-        print("Invalid output directory to store results.")
-        parser.print_help()
+        utils.ERROR("Invalid directory path to 10x Genomics data files.")
+
+    if args.output and not os.path.isdir(args.output):
+        utils.ERROR("Invalid output directory to store results.")
+
     if args.min_genes is not None and args.min_genes < 0:
-        print("Minimum number of genes expressed per cell (-g, --min_genes) must be greater than 0.")
+        utils.ERROR("Minimum number of genes expressed per cell (-g, --min_genes) must be greater than 0.")
+
     if args.min_cells is not None and args.min_cells < 0:
-        print("Minimum number of cells expressing a gene (-c, --min_cells) must be greater than 0.")
+        utils.ERROR("Minimum number of cells expressing a gene (-c, --min_cells) must be greater than 0.")
+
     if args.min_cell_reads is not None and args.min_cell_reads < 0:
-        print("Minimum number of reads per cell (-cr, --min_cell_reads) must be greater than 0.")
+        utils.ERROR("Minimum number of reads per cell (-cr, --min_cell_reads) must be greater than 0.")
+
     if args.min_gene_counts is not None and args.min_gene_counts < 0:
-        print("Minimum number of counts per gene (-gc, --min_gene_counts) must be greater than 0.")
+        utils.ERROR("Minimum number of counts per gene (-gc, --min_gene_counts) must be greater than 0.")
+
     if args.n_top_genes is not None and args.n_top_genes < 0:
-        print("Number of reads per cell (-t, --target_sum) must be greater than 0.")
+        utils.ERROR("Number of highly variable genes to keep (-ntop, --n_top_genes) must be greater than 0.")
+
     if args.target_sum is not None and args.target_sum < 0:
-        print("Number of reads per cell (-t, --target_sum) must be greater than 0.")
+        utils.ERROR("Number of reads per cell for normalization (-t, --target_sum) must be greater than 0.")
+
     if args.nComp is not None and args.nComp < 0:
-        print("Number of PCs (-n, --nComp) must be greater than 0.")
+        utils.ERROR("Number of principal components (-n, --nComp) must be greater than 0.")
 
     # -------------------- set up output and start analysis --------------------
- 
+
     if args.output is None:
         outdir = os.getcwd()
     else:
         outdir = args.output
-    
+
     if outdir[-1] != "/":
         outdir += "/"
     if not os.path.exists(outdir):
         os.mkdir(outdir)
     outf = open(outdir + "scpydr_results.txt", "w")
-    
+
     sys.stdout.write("Welcome to scPyDR! Starting analysis.\n\n")
 
     # -------------------- load and preprocess data --------------------
@@ -135,8 +141,8 @@ def main():
                     and 2) directory contains properly formatted 10x Genomics files. \
                     Read more on 10x Genomics files here: https://www.10xgenomics.com/support/software/space-ranger/latest/advanced/hdf5-feature-barcode-matrix-format")
     try:
-        adata = utils.preprocess(adata, args.min_genes, args.min_cells, args.min_cell_reads, \
-                                    args.min_gene_counts, args.n_top_genes, args.target_sum)
+        adata = utils.preprocess(adata, args.min_genes, args.min_cells, args.min_cell_reads,
+                                 args.min_gene_counts, args.n_top_genes, args.target_sum)
     except Exception:
         utils.ERROR(">> Failed to preprocess data. Check arguments and try again.")
     try:
@@ -152,20 +158,20 @@ def main():
 
     nComp = None
     if args.nComp is None:
-        #for n data points and m features, there are min(n-1,m) PCs.
+        # for n data points and m features, there are min(n-1,m) PCs.
         num_pts = df.shape[0] * df.shape[1]
         num_features = df.shape[1]
         nComp = min(num_pts - 1, num_features)
     else:
         nComp = args.nComp
-        
-    pca = utils.scpydrPCA(nComp=nComp) #create pca object
+
+    pca = utils.scpydrPCA(nComp=nComp)  # create pca object
     sys.stdout.write("PCA object created. \n")
-    pca.fit(df) #compute new PCs
+    pca.fit(df)  # compute new PCs
     sys.stdout.write("New PCs computed. \n")
-    df = pca.transform(df) #fit data to new PCs
+    df = pca.transform(df)  # fit data to new PCs
     sys.stdout.write("Original data successfully projected onto the new PCs! \n\n")
-    
+
     if args.umap:
         sys.stdout.write("Running UMAP for dimensionality reduction and visualization... \n")
         umap_embedding = utils.umap_embedding(adata)
