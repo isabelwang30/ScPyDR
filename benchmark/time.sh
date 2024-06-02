d81252c860ebaec481a3fcd94539306452d84f90
isabelwang30 <103291789+isabelwang30@users.noreply.github.com>
2024-06-02 14:42:27 -0700

    updated timing script

--- benchmark/time.sh
+++ benchmark/time.sh
@@ -1,3 +1,4 @@
+# run with command: bash benchmark/time.sh
 #!/bin/bash
 
 # Make this script executable
@@ -5,16 +6,16 @@ chmod +x "$0"  # "$0" represents the path to the current script
 
 # Define input variables
 DATADIR="benchmark"
-OUTPUT_DIR="scpydr_results"
 
 # Run scpydr PCA
 echo "Running scpydr PCA..."
-mkdir -p $OUTPUT_DIR
-time scpydr benchmark -o $OUTPUT_DIR
+TOTAL_SCPYDR_TIME=$(echo $( TIMEFORMAT="%3U + %3S"; { time scpydr $DATADIR -o $DATADIR/scpydr_results; } 2>&1) | bc -l)
+echo "Total scpydr PCA time: $TOTAL_SCPYDR_TIME seconds"
 
 # Run scanpy PCA
 echo "Running scanpy PCA..."
-time python scanpy_pca_script.py $DATADIR -o $OUTPUT_DIR
+TOTAL_SCANPY_TIME=$(echo $( TIMEFORMAT="%3U + %3S"; { time python $DATADIR/scanpy_pca.py $DATADIR -o $DATADIR/scanpy_results; } 2>&1) | bc -l)
+echo "Total scanpy PCA time: $TOTAL_SCANPY_TIME seconds"
 
 # Compare results if needed
 # Add your comparison logic here
