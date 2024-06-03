833d346e808151364f9ff4164d28ec31b3a7e342
isabelwang30 <103291789+isabelwang30@users.noreply.github.com>
2024-06-03 09:59:24 -0700

    update with changes from main, ran analysis on public dataset

--- build/lib/scPyDR/scPyDR.py
+++ build/lib/scPyDR/scPyDR.py
@@ -77,10 +77,6 @@ def main():
                         help="Run UMAP for dimensionality reduction and visualization.", 
                         action="store_true", 
                         required=False)
-    parser.add_argument("-v", "--visualize", 
-                        help="Visualize the results of scPyDR.", 
-                        action="store_true", 
-                        required=False)
 
     # parse args
     args = parser.parse_args()
@@ -192,10 +188,10 @@ def main():
         umap_embedding, cluster_labels = utils.umap_embedding(adata)
         # Further actions with umap_embedding if needed
         sys.stdout.write("UMAP computation completed! \n\n")
-        if args.visualize:
-            # Plot the UMAP embedding
-            sys.stdout.write("Plotting UMAP embedding... \n")
-            utils.plot_umap_results(outdir, filename_prefix, umap_embedding, cluster_labels)
+
+        # Plot the UMAP embedding
+        sys.stdout.write("Plotting UMAP embedding... \n")
+        utils.plot_umap_results(outdir, filename_prefix, umap_embedding, cluster_labels)
 
     # -------------------- analysis and conclusion --------------------
 
